2d939b84c40d69d0fd68c94da6112475641ee9af
Juan Santiago Acevedo <js.acevedo10@uniandes.edu.co>
2018-05-14 23:08:06 -0500

    Archivo properties y visualizacion

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ var co = require('co');
 var prompt = require('co-prompt');
 var program = require('commander');
 var cmd = require('node-cmd');
+var propertiesReader = require('properties-reader');
+var firebase = require('firebase');
+require('@firebase/database');
 const { exec } = require('child_process');
 var apkDirectory = '';
 var maxMutants = 0;
+var firebaseCounter = 0;
+var initTime = Date();
 var results = {
     goodMonkeys : 0,
     badMonkeys : 0,
     monkeyResults: [],
-    mutantDescriptions : []
+    mutantDescriptions : [],
+    unitResults : [],
+    firebaseTestResults : []
 }
 
 function Stack() {
@@ -112,7 +119,7 @@ function createMutants() {
         mkdir -p output/apks/
         cd ${androidStudioPath}/app/build/outputs/apk/debug
         pwd
-    `, function(err, data, stderr) {
+    `, function(err, data, stderr2) {
         if (!err) {
             apkDirectory = data;
             for (var i = 0; i < numOfMutants; i++) {
@@ -128,35 +135,51 @@ function createMutants() {
                     cd ${x}
                     chmod +x gradlew
                     ./gradlew test
-                    ./gradlew assembleDebug
-                `, function(err, data, stderr) {
-                    if (!err) {
-                        // console.log(data);
-                        cmd.get(`
-                            cd ${androidStudioPath}
-                            cd ../
-                            cd ./${x}/app/build/outputs/apk/debug
-                            mv app-debug.apk ${mdroidFolder}/output/apks/
-                            cd ${mdroidFolder}/output/apks/
-                            mv "app-debug.apk" "app-mutant-${x}.apk"
-                            cd ${androidStudioPath}
-                            cd ../
-                            rm -rf ${x}
-                        `, function(err, data, stderr) {
-                            if(!err) {
-                                if (x == numOfMutants - 1) {
-                                    runMonkeys(0)
+                `, function(err1, data1, stderr1) {
+                    if(err1) {
+                        console.log('errooooooooor', err1);
+                        results.unitResults[x] = false
+                        // restoreOriginalContent();
+                    } else {
+                        console.log(data1);
+                        results.unitResults[x] = true
+                    }
+                    cmd.get(`
+                        cd ${androidStudioPath}
+                        cd ../
+                        cd ${x}
+                        ./gradlew assembleDebug
+                    `, function(err2, data2, stderr2) {
+                        if (!err2) {
+                            // console.log(data);
+                            // rm -rf ${x}
+                            cmd.get(`
+                                cd ${androidStudioPath}
+                                cd ../
+                                cd ./${x}/app/build/outputs/apk/debug
+                                mv app-debug.apk ${mdroidFolder}/output/apks/
+                                cd ${mdroidFolder}/output/apks/
+                                mv "app-debug.apk" "app-mutant-${x}.apk"
+                                cd ${androidStudioPath}
+                                cd ../                                    
+                            `, function(err3, data3, stderr3) {
+                                if(!err3) {
+                                    if (x == numOfMutants - 1) {
+                                        setTimeout(function () {
+                                            runMonkeys(0);
+                                        }, 5000);
+                                        restoreOriginalContent();
+                                    }
+                                } else {
+                                    console.log('error', err3);
                                     restoreOriginalContent();
                                 }
-                            } else {
-                                console.log('error', err);
-                                restoreOriginalContent();
-                            }
-                        });
-                    } else {
-                        console.log('error', err);
-                        restoreOriginalContent();
-                    }
+                            });
+                        } else {
+                            console.log('error', err2);
+                            restoreOriginalContent();
+                        }
+                    })
                 })
             }
         } else {
@@ -166,34 +189,34 @@ function createMutants() {
     })
 }
 
+function startEmulator() {
+    cmd.run('cd ${ANDROID_HOME}/tools && ./emulator @Nexus5');
+}
+
 function runMonkeys(x) {
     console.log(`\n\nRunning monkey ${x}`);
         cmd.get(`
             cd ${mdroidFolder}/output/apks/
-            emulator @Nexus5 -gpu on
             adb install -r app-mutant-${x}.apk
-            adb shell monkey -p ${packageName} -s ${x}919 -v -v 100
+            adb shell monkey -p ${packageName} -s ${x}919 -v -v ${numOfMonkeyEvents}
         `, function(err, data, stderr) {
             if(!err) {
                 results.goodMonkeys++
-                results.monkeyResults[x] = 'PASSED';
+                results.monkeyResults[x] = true;
             } else {
                 console.log('\nMico malo !!\n', err);
                 results.badMonkeys++;
-                results.monkeyResults[x] = `FAILED`;
+                results.monkeyResults[x] = false;
+            }
+            if (firebaseTestLab) {
+                runFirebaseTestLab(x);
             }
             if (x < numOfMutants - 1) {
-                runFirebaseTestLab(x);
                 runMonkeys(x + 1);
             } else {
-                console.log('=============================\n');
-                console.log('Resultados:');
-                console.log('Monkey tests passed: ' + results.goodMonkeys);
-                console.log('Monkey tests failed: ' + results.badMonkeys);
-                for (var i = 0; i < numOfMutants; i++) {
-                    console.log(`Mutant ${i+1}: ${results.mutantDescriptions[i]}\t(${results.monkeyResults[i]})`);
+                if (!firebaseTestLab) {
+                    generateResults();
                 }
-                console.log('\n=============================');
             }
         });
 }
@@ -202,33 +225,93 @@ function runFirebaseTestLab(x) {
     console.log('\n\nRunning Firebase Test Lab');
     cmd.get(`
         cd ${mdroidFolder}/output/apks/
-        gcloud firebase test android run --app app-mutant-${x}.apk --device model=Nexus6,version=21,locale=en,orientation=portrait --timeout 90s
+        gcloud firebase test android run --app app-mutant-${x}.apk --device model=Nexus6,version=21,locale=en,orientation=portrait --timeout ${firebaseTestDuration}
     `, function (err, data, stderr) {
+        firebaseCounter++;
         if(!err) {
             console.log(data);
+            results.firebaseTestResults[i] = data.includes('Passed');
         } else {
             console.log(`Error\n\n: ${stderr}`);
+            results.firebaseTestResults[x] = false;
+        }
+        if(firebaseCounter == numOfMutants) {
+            generateResults();
         }
     });
 }
 
-program
-    .version('0.0.1')
-    .arguments('<android_studio_path> <package_name> <app_name> <mdroid-folder> [num_of_mutants]')
-    .action(function(android_studio_path, package_name, app_name, mdroid_folder, num_of_mutants) {
-        co(function *() {
-            androidStudioPath = android_studio_path.replace(/ /g,"\\ ");
-            packageName = package_name.replace(/ /g,"\\ ");
-            appName = app_name.replace(/ /g,"\\ ");
-            mdroidFolder = mdroid_folder.replace(/ /g,"\\ ");
-            mutantsPath = 'tmp/mutants/';
-            if (num_of_mutants !== undefined) {
-                numOfMutants = num_of_mutants;
-            } else {
-                numOfMutants = 5;
-                console.log('5 mutantes');
+function generateResults() {
+    initFirebase();
+    console.log('=============================\n');
+    console.log('Results:');
+    for (var i = 0; i < numOfMutants; i++) {
+        console.log(`Mutant ${i+1}:
+        Description: ${results.mutantDescriptions[i]}`);
+        var unit = 'FAILED';
+        if (results.unitResults[i]) {
+            unit = 'PASSED';
+        }
+        console.log(`\tUnit tests: ${unit}`)
+        var monkey = 'FAILED';
+        if (results.monkeyResults[i]) {
+            monkey = 'PASSED';
+        }
+        console.log(`\tMonkey test: ${monkey}`)
+        if(firebaseTestLab) {
+            var fire = 'FAILED';
+            if (results.firebaseTestResults[i]) {
+                fire = 'PASSED';
             }
-        })
+            console.log(`\tFirebase Test Lab: ${fire}`)
+        }
+    }
+    console.log('\n=============================');
+}
+
+function initFirebase() {
+    var config = {
+        apiKey: "AIzaSyC7V_YT9u-hKqjB9o_QzfOG9nrvNi4oU5Q",
+        authDomain: "hitpa-testing-3a.firebaseapp.com",
+        databaseURL: "https://hitpa-testing-3a.firebaseio.com",
+        storageBucket: "hitpa-testing-3a.appspot.com",
+    };
+    firebase.initializeApp(config);
+    writeTestData(results);
+}
+
+function writeTestData(testResult) {
+    testResult.initTimestamp = initTime;
+    testResult.finishTimestamp = Date();
+    testResult.appName = appName;
+    testResult.packageName = packageName;
+    testResult.numOfMonkeyEvents = numOfMonkeyEvents;
+    testResult.firebaseTestLab = firebaseTestLab;
+    testResult.firebaseTestDuration = firebaseTestDuration;
+    testResult.numOfMutants = numOfMutants;
+    var ref = firebase.database().ref('tests/').push();
+    ref.set(testResult);
+    console.log('\n=============================');
+    console.log(`Visita https://hitpa.tresastronautas.com para ver el resultado de tu prueba al detalle (ID = ${ref})`);
+    console.log('=============================');
+}
+
+program
+    .version('1.0')
+    .arguments('<properties_path>')
+    .action(function(properties_path) {
+        co(function *() {
+            properties = propertiesReader(properties_path);
+            androidStudioPath = properties.get('android_studio_path').replace(/ /g,"\\ ");
+            packageName = properties.get('package_name').replace(/ /g,"\\ ");
+            appName = properties.get('app_name').replace(/ /g,"\\ ");
+            mdroidFolder = properties.get('mdroid_folder').replace(/ /g,"\\ ");
+            mutantsPath = properties.get('mutants_folder').replace(/ /g,"\\ ");
+            numOfMutants = properties.get('num_of_mutants');
+            numOfMonkeyEvents = properties.get('num_of_monkey_events');
+            firebaseTestLab = properties.get('firebase_test_lab');
+            firebaseTestDuration = properties.get('firebase_test_duration');
+        });
     });
 program.parse(process.argv);
 
@@ -250,6 +333,16 @@ if (typeof packageName === 'undefined') {
 if (typeof mdroidFolder === 'undefined') {
     console.error('mDroid Folder not recognized.');
     process.exit(1);
+}
+
+if (numOfMutants > 20) {
+    console.error('El máximo de mutantes a probar es 20.');
+    numOfMutants == 20;
+}
+
+if (numOfMonkeyEvents > 10000) {
+    console.error('El máximo de eventos Monkey es de 10000.');
+    numOfMutants == 5000;
 }
 
 installMDroid();
